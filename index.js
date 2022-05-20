daf5a246ff54f379da697f2c43f6a6a4b71893f1
AdolfoCBasilio <adolfo_basiliogeniz@hotmail.com>
2022-05-20 00:16:15 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById('login').addEventListener('click',(e)=>{
+    e.preventDefault()
+    window.location.assign('https://www.linkedin.com/in/adolfocbg/')
+})
+document.getElementById('createaccount').addEventListener('click',(e)=>{
+    e.preventDefault()
+    window.location.assign('https://www.linkedin.com/in/adolfocbg/')
+})
